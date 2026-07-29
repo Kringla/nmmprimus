1db681d091c1dbedadd47d0e89b34141ff362b50
Kringla <gerhard@ihlen.net>
2026-07-29 10:06:19 +0200

    Index

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 declare(strict_types=1);
-
+require_once __DIR__ . '/session_check.php';
 require_once __DIR__ . '/includes/auth.php';
 require_once __DIR__ . '/includes/functions.php';
 require_once __DIR__ . '/config/constants.php';
